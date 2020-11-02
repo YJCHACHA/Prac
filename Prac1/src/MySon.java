baf582c9703c6b852c52c9b8d50ef1b74a6a1f0a
CYJ <cha8967@naver.com>
2020-11-02 14:35:42 +0900

    하위 클래스에서 상위 클래스의 private 멤버에 접근할 때 public method 사용

--- Prac1/src/MySon.java
+++ Prac1/src/MySon.java
@@ -2,7 +2,8 @@
 public class MySon extends MyFather{
 	public void sayFatherNames() {
 		System.out.println(name);
-		//System.out.println(nickname);
+		//System.out.println(nickname); //에러 발생
+		System.out.println(getNickName());
 	}
 
 	public static void main(String[] args) {
